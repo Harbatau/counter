ea52e34158476f87e316410439493cdb4a4da113
Harbatau <npikolist@gmail.com>
2020-02-03 21:49:49 +0300

    before sleep

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -7,21 +7,27 @@ const Settings = (props) => {
 
     return (
         <div className={'settings'}>
-            <SettingsInput blur={(e) => props.onBlurHandler(e, 'maxInput')}
-                           value={props.maxInputValue} inputName={'max value'}
-                           change={(e) => {props.updateValuesFromInputs(e, 'maxInput')}}
-            />
-            <SettingsInput blur={(e) => props.onBlurHandler(e, 'minInput')}
-                           value={props.minInputValue} inputName={'min value'}
-                           change={(e) => {props.updateValuesFromInputs(e, 'minInput')}}
-            />
+            <div className={'flex settingsInputs'}>
+                <SettingsInput blur={(e) => props.onBlurHandler(e, 'maxInput')}
+                               value={props.maxInputValue} inputName={'max value'}
+                               change={(e) => {
+                                   props.updateValuesFromInputs(e, 'maxInput')
+                               }}
+                />
+                <SettingsInput blur={(e) => props.onBlurHandler(e, 'minInput')}
+                               value={props.minInputValue} inputName={'min value'}
+                               change={(e) => {
+                                   props.updateValuesFromInputs(e, 'minInput')
+                               }}
+                />
+            </div>
             <ControlButton onClickHandler={props.setValues}
-                           buttonProperty={'Set options'}
+                           buttonProperty={'set options'}
                            disableMonitor={(props.isSettingButtonNotReady ||
                                props.isNumberValuesNotValid)}
             />
-            <div className={props.isNumberValuesNotValid ? 'alertMessage' : 'displayNone'}>
-                Максимальное число не может быть меньше минимального!
+            <div className={/*props.isNumberValuesNotValid ? 'alertMessage' : 'displayNone'*/'alertMessage'}>
+                minValue cannot be greater than maxValue!
             </div>
         </div>
     )
