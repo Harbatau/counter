a4e77be1030467d331a98f8f0f2c60c94e68e5c1
Harbatau <npikolist@gmail.com>
2020-02-08 21:33:28 +0300

    08022020

--- src/components/counterDisplay.jsx
+++ src/components/counterDisplay.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 import '../Assets/App.sass';
 
 const CounterDisplay = (props) => {
+
+    let svgClass = props.alertMonitor ? 'text-copy' : 'text-copy-alert';
+
     return (
         <div className={'counterDisplay'}>
-            <div className={props.alertMonitor ? 'number' : 'number alertNumber'}>{props.counterNumber}</div>
+            <div className={props.alertMonitor ? 'number' : 'number alertNumber'}>
+                {props.isFirstVersion ? props.counterNumber :
+                    <svg viewBox="0 0 960 600">
+                        <symbol id="s-text">
+                            <text textAnchor="middle" x="50%" y="80%">{props.counterNumber}</text>
+                        </symbol>
+                        <g className="g-ants">
+                            <use xlinkHref="#s-text" className={svgClass}></use>
+                            <use xlinkHref="#s-text" className={svgClass}></use>
+                            <use xlinkHref="#s-text" className={svgClass}></use>
+                            <use xlinkHref="#s-text" className={svgClass}></use>
+                            <use xlinkHref="#s-text" className={svgClass}></use>
+                        </g>
+                    </svg>}
+            </div>
             <div className={'flex'}>
                <div className={'currentSettings'}>Current max value:
                     <div className={'currSetVal'}>{props.maxValue}</div>
