a7f6eb9eb1b7d91a32fa62d993f664a0394d9b0e
Harbatau <npikolist@gmail.com>
2020-02-09 14:45:41 +0300

    Updates

---
+++ precache-manifest.f3ced507684050b28889ec657d8ec602.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86921b7ecf15723ece38337d720089b6",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "b885e04da76a9b593d0b",
+    "url": "/counter/static/css/main.cef221a9.chunk.css"
+  },
+  {
+    "revision": "b14aaf2f0b13101a9873",
+    "url": "/counter/static/js/2.c65dc71c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/counter/static/js/2.c65dc71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b885e04da76a9b593d0b",
+    "url": "/counter/static/js/main.91865277.chunk.js"
+  },
+  {
+    "revision": "398aca49dd582097750b",
+    "url": "/counter/static/js/runtime-main.068fc69c.js"
+  },
+  {
+    "revision": "257bdb11a1e1e71fdb06f6a2c80af12e",
+    "url": "/counter/static/media/Anders.257bdb11.ttf"
+  },
+  {
+    "revision": "8d758b6aa240f6ad89f575394f624777",
+    "url": "/counter/static/media/Gradient-Mesh-27.8d758b6a.jpg"
+  }
+]);
